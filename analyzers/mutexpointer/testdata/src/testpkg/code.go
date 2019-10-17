6ec6bb355956ee874a6650f818b6d9c6cb03f05c
Mir Shahriar Sabuj <shahriarsabuj@pathao.com>
2019-10-17 06:34:05 +0600

    fix comments

--- analyzers/mutexpointer/testdata/src/testpkg/code.go
+++ analyzers/mutexpointer/testdata/src/testpkg/code.go
@@ -8,11 +8,11 @@ var lock *sync.Mutex // want "mutex-pointer: var lock uses pointer to sync.Mutex
 var x, y *sync.Mutex // want "mutex-pointer: var x uses pointer to sync.Mutex" "mutex-pointer: var y uses pointer to sync.Mutex"
 
 var (
-	invalidMutextInBlock *sync.Mutex // want "mutex-pointer: var invalidMutxtInBlock uses pointer to sync.Mutex"
+	invalidMutextInBlock *sync.Mutex // want "mutex-pointer: var invalidMutextInBlock uses pointer to sync.Mutex"
 	validMutex            sync.Mutex
 )
 
-type rwm *sync.RWMutex // want "mutex-pointer: type rwm uses pointer to sync.Mutex"
+type rwm *sync.RWMutex // want "mutex-pointer: type rwm uses pointer to sync.RWMutex"
 
 func testFunc() {
 	var anotherMutexPointer *sync.Mutex // want "mutex-pointer: var anotherMutexPointer uses pointer to sync.Mutex"
